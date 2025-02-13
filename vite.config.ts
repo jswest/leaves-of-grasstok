1be519f28c8e5c2e94af1220c304444b02e07fd7
John West <john@beebe-west.com>
2025-02-13 12:15:27 -0500

    Update the data json

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
-};
+export default defineConfig({
+	plugins: [sveltekit()]
+});
