30f6ef701f61d4f286e94be72aa1291be1ccac17
John West <john@beebe-west.com>
2025-02-14 05:52:20 -0500

    Attempt to add tracking

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,6 @@
 export const prerender = true;
+
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+ 
+injectAnalytics({ mode: dev ? 'development' : 'production' });
